3edd47d0aa35ac11be8efc9ae1ad31288c1abe0a
Christian Koop <contact@sprax2013.de>
2021-04-15 07:04:29 +0200

    Restructure and rename project

--- src/get-sysinternals-du.js
+++ src/get-sysinternals-du.js
@@ -3,5 +3,5 @@ const path = require('path')
 const unzipper = require('unzipper')
 
 https.get('https://download.sysinternals.com/files/DU.zip', function (res) {
-  res.pipe(unzipper.Extract({ path: path.join(__dirname, 'bin') }))
+  res.pipe(unzipper.Extract({ path: path.join(__dirname, '..', 'bin') }))
 })
